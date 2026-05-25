ae8253fb603b8f40ef869414f8de871dce1739d3
root <root@runnervmg397c.oc0tmsjkidze5cmwmr3zb3bgga.bx.internal.cloudapp.net>
2026-05-25 11:03:43 +0000

    Revert "lk2nd: msm8953: Add Yureka 2 (taco) display panel support"
    This reverts commit d607fdd3fe0c88f0182ffc19101c388514256df4.

--- lk2nd/display/panel/oem_panel.c
+++ lk2nd/display/panel/oem_panel.c
@@ -63,5 +63,6 @@ int oem_panel_select(const char *panel_name, struct panel_struct *panel,
 	else
 		memcpy(panel_regulator_settings, dcdc_regulator_settings, REGULATOR_SIZE);
 #endif
+
 	return PANEL_TYPE_DSI;
 }
